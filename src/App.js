2a9beab5e4a265d60bf71a56ace68bced02b6478
lameburger <laneburgett01@gmail.com>
2025-02-05 22:05:58 -0600

    app sf

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import './App.css'; // Import the CSS file to apply the background override
 import MarketingPage from './pages/MarketingPage';
 import TermsOfService from './pages/TermsOfService';
 import PrivacyPolicy from './pages/PrivacyPolicy';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark', // This sets the theme to dark mode
+  },
+});
 
 function App() {
   return (
